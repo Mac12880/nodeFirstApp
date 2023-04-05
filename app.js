06cd4fc1ce03c1ea2cc9e0aec5515c26d8af2fdb
Mac12880 <marc.juan6.mj@gmail.com>
2023-04-05 16:32:46 +0800

    practice

---
+++ app.js
@@ -0,0 +1,21 @@
+// function sayHello(name){
+//     console.log(`Hello ${name}`);
+// }
+
+// setTimeout(sayHello("Marc"), 10000);
+
+// console.log(window);
+
+// let message = "Hi";
+
+// console.log(global.message);
+
+// console.log(module);
+// console.log(logger);
+
+//to load a module
+let logger = require('./logger.js');
+
+
+
+logger.log('message');
