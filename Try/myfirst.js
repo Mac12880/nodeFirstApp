44f21fe5c38869e708fe0c8e910cdd0ccebf466e
Mac12880 <marc.juan6.mj@gmail.com>
2023-04-07 19:43:30 +0800

    check time through a created module

--- Try/myfirst.js
+++ Try/myfirst.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
-http.createServer(function(req,res){
+// http.createServer(function(req,res) {
+//     res.writeHead(200, {'Content-Type' : 'text/html'});
+//     res.end('Hello World');
+// }).listen(8080);
+
+var dt = require('./myfirstmodule');
+
+http.createServer(function(req,res) {
     res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.end('Hello World');
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
 }).listen(8080);
