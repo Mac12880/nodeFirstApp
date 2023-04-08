c33dbdf7c3243e68c230da6f558903a6ad73b7bf
Mac12880 <marc.juan6.mj@gmail.com>
2023-04-08 12:08:33 +0800

    fs append

--- Try/demo_filesystem.js
+++ Try/demo_filesystem.js
@@ -1,11 +1,21 @@
 var fs = require('fs');
 
-fs.appendFile('mynewfile1.txt', 'Hello content!', function (err){
-    if (err) throw err;
-    console.log('Saved!');
-});
+// fs.appendFile('mynewfile1.txt', 'Hello content! mynewfile1.txt', function (err){
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
 
-fs.open('mynewfile2.txt', 'w', function (err, file){
+// fs.open('mynewfile2.txt', 'w', function (err, file){
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
+
+// fs.writeFile('mynewfile3.txt', 'Hello Content! mynewfile3.txt', function (err){
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
+
+fs.appendFile('mynewfile1.txt', 'This is the new text', function (err){
     if (err) throw err;
-    console.log('Saved!');
+    console.log('Updated!');
 });
