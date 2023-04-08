bf0ea8bfc392b8d5a8012428e6b791e2fff5714e
Mac12880 <marc.juan6.mj@gmail.com>
2023-04-08 12:43:41 +0800

    delete a file using unlink

--- Try/demo_filesystem.js
+++ Try/demo_filesystem.js
@@ -20,7 +20,12 @@ var fs = require('fs');
 //     console.log('Updated!');
 // });
 
-fs.writeFile('mynewfile3.txt', 'This is my content', function (err) {
+// fs.writeFile('mynewfile3.txt', 'This is my content', function (err) {
+//     if (err) throw err;
+//     console.log('Replaced!');
+// });
+
+fs.unlink('mynewfile2.txt', function (err) {
     if (err) throw err;
-    console.log('Replaced!');
+    console.log('File deleted!');
 });
