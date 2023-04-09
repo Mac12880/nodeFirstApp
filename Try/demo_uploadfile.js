31240fd8d0da54050729764369eed5860b17d415
Mac12880 <marc.juan6.mj@gmail.com>
2023-04-09 22:00:08 +0800

    working upload file

--- Try/demo_uploadfile.js
+++ Try/demo_uploadfile.js
@@ -1,10 +1,27 @@
 var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"<br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    res.end();
+    
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            var oldpath = files.filetoupload.filepath;
+            var newpath = 'C:/Users/Marc/Documents/Github/nodeFirstApp/Try/' + files.filetoupload.originalFilename;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved');
+                res.end();
+            });
+        });
+    }
+    else {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"<br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        res.end();
+    }
 }).listen(8080);
